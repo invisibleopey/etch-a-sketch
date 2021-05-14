aaf267f9309037464fd94bdb004fbf521d935e94
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-05-14 21:35:30 +0100

    Added a function to color the divs on hover

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('#container');
-
 // Creates 16 x 16 divs inside the Div Container
 function createDivs () {
     for (let i = 1; i <= 256; i++) {
@@ -9,3 +8,17 @@ function createDivs () {
 
 // Call the function to create the divs
 createDivs();
+
+
+// Coloring the Boxes
+const boxes = document.querySelectorAll('.box');
+// Add event listener to the boxes
+boxes.forEach(box => box.addEventListener('mouseover', colorDiv));
+
+function colorDiv (e) {
+    let redValue = Math.floor(Math.random() * 255);
+    let greenValue = Math.floor(Math.random() * 255);
+    let blueValue = Math.floor(Math.random() * 255);
+    e.target.style.backgroundColor = "rgb(" + redValue + "," + 
+    greenValue + "," + blueValue + ")";
+}
