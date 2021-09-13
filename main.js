9511c805f0398d74982c6ac8e6f05550f0fe32a2
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-13 22:23:56 +0100

    Applied prettier to refactor the formatting

--- main.js
+++ main.js
@@ -1,54 +1,52 @@
 const container = document.querySelector('#container');
 // Creates 16 x 16 divs inside the Div Container
-function createDivs () {
-    for (let i = 1; i <= 256; i++) {
-        container.innerHTML += '<div class="box">  </div>';
-    }
+function createDivs() {
+  for (let i = 1; i <= 256; i++) {
+    container.innerHTML += '<div class="box">  </div>';
+  }
 }
 
 // Call the function to create the divs
 createDivs();
 
-
 // Coloring the Boxes
 const boxes = document.querySelectorAll('.box');
 // Add event listener to the boxes
-boxes.forEach(box => box.addEventListener('mouseover', colorDiv));
+boxes.forEach((box) => box.addEventListener('mouseover', colorDiv));
 
-function colorDiv (e) {
-    let redValue = Math.floor(Math.random() * 255);
-    let greenValue = Math.floor(Math.random() * 255);
-    let blueValue = Math.floor(Math.random() * 255);
-    e.target.style.backgroundColor = "rgb(" + redValue + "," + 
-    greenValue + "," + blueValue + ")";
+function colorDiv(e) {
+  let redValue = Math.floor(Math.random() * 255);
+  let greenValue = Math.floor(Math.random() * 255);
+  let blueValue = Math.floor(Math.random() * 255);
+  e.target.style.backgroundColor = 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')';
 }
 
 // Button that clears and reset the sizes
-const btn = document.querySelector("#btn");
+const btn = document.querySelector('#btn');
 btn.addEventListener('click', changeSize);
 
-let selection = "";
-function changeSize () {
-    // Ask number from user, keep on asking until its a number
-    do{
-        selection = parseInt(prompt("Please enter a number from 1 to 30", ""), 10);
-    }while(isNaN(selection) || selection > 30 || selection < 1);
-    // Remove all previous Divs
-    removeAllChildNodes(container);
-    // create divs using the inputed number
-    for (let i = 1; i <= selection ** 2; i++) {
-        container.innerHTML += '<div class="box">  </div>';
-    }
-    // Style 
-    container.style.gridTemplateColumns = "repeat(" + selection+ ", 2fr)";
-    container.style.gridTemplateRows = "repeat(" + selection+ ", 2fr)";
-    // Color the new boxes
-    const newBoxes = document.querySelectorAll('.box');
-    newBoxes.forEach(box => box.addEventListener('mouseover', colorDiv));
+let selection = '';
+function changeSize() {
+  // Ask number from user, keep on asking until its a number
+  do {
+    selection = parseInt(prompt('Please enter a number from 1 to 30', ''), 10);
+  } while (isNaN(selection) || selection > 30 || selection < 1);
+  // Remove all previous Divs
+  removeAllChildNodes(container);
+  // create divs using the inputed number
+  for (let i = 1; i <= selection ** 2; i++) {
+    container.innerHTML += '<div class="box">  </div>';
+  }
+  // Style
+  container.style.gridTemplateColumns = 'repeat(' + selection + ', 2fr)';
+  container.style.gridTemplateRows = 'repeat(' + selection + ', 2fr)';
+  // Color the new boxes
+  const newBoxes = document.querySelectorAll('.box');
+  newBoxes.forEach((box) => box.addEventListener('mouseover', colorDiv));
 }
 
 function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
